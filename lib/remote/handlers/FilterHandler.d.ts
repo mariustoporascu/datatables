e2a1338bb16f143a8d0e599b106ed70667b5de93
Marius Toporascu <mariustoporascu@topodvlp.ro>
2024-04-03 22:43:48 +0300

    build

---
+++ lib/remote/handlers/FilterHandler.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="svelte" />
+import type { Filter } from '..';
+import type Context from '../Context';
+import type { Writable } from 'svelte/store';
+export default class FilterHandler<Row> {
+    filters: Writable<Filter<Row>[]>;
+    constructor(context: Context<Row>);
+    set(value: string | number, filterBy: keyof Row): void;
+    remove(): void;
+}
